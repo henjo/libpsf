3543906ac1642f7b9a13e98a11273daa89154008
Henrik Johansson <henrik@johome.net>
2011-03-08 22:19:17 +0100

    changed get_signal_vector -> get_signal

--- examples/python/tran.py
+++ examples/python/tran.py
@@ -1,13 +1,12 @@
 import libpsf
 
-#d = libpsf.PSFDataSet("/nfs/home/henrik/pycircuit.repo/pycircuit/post/cds/test/psf/tran.tran") 
 d = libpsf.PSFDataSet("../data/timeSweep")
 print d.get_header_properties()
 
 
 print list(d.get_signal_names())
 t = d.get_sweep_values()
-v = d.get_signal_vector('INP')
+v = d.get_signal('INP')
 
 print len(v), len(t)
 
