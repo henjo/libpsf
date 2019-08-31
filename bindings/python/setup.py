b0775ba21b9c840ffba51a33a826d323950ad4c2
ota2 <lekez2005@yahoo.ca>
2019-08-31 02:15:39 -0400

    Statically build libpsf for distribution

--- bindings/python/setup.py
+++ bindings/python/setup.py
@@ -21,9 +21,21 @@ except:
 
 root_include = os.path.abspath(os.path.join(get_paths()['include'], '..'))
 
+# https://stackoverflow.com/questions/4597228/how-to-statically-link-a-library-when-compiling-a-python-module-extension
+lib_dir = os.path.abspath(os.path.join(get_paths()['stdlib'], '..'))
+if 'bdist_wheel' in sys.argv:
+    static_libraries = ['psf']
+    extra_objects = ['{}/lib{}.a'.format(lib_dir, l) for l in static_libraries]
+    libraries = []
+else:
+    extra_objects = []
+    libraries = ['psf']
+
+
 libpsf_ext = Extension(
     name="libpsf",
     sources = ["libpsf.pyx", "psfpython.cc"],
+    extra_objects = extra_objects,
     libraries = ["psf"],
     include_dirs = [root_include, numpy.get_include() ],
 )
