262109bb8bdb8002c5b4c7733905fc7c9def467a
ota2 <lekez2005@yahoo.ca>
2021-04-19 21:05:22 -0400

    psftestraw checkpoint

--- src/psftestraw.cc
+++ src/psftestraw.cc
@@ -5,18 +5,23 @@
 int main(int argc, char *argv[]) {
     char * filename = argv[1];
 
-    PSFDataSet data(filename);
-    data.open();
+    try{
+        PSFDataSet data(filename); 
+        data.open();
+    
 
-    PSFDoubleVector* vout = (PSFDoubleVector *) data.get_signal_vector("vout");
+        PSFDoubleVector* vout = (PSFDoubleVector *) data.get_signal_vector("i(vvdd)");
 
-    for (auto prop: data.get_header_properties()){
-        std::cout << prop.first << ": \t" << *prop.second << std::endl;
+        // for (auto prop: data.get_header_properties()){
+        //     std::cout << prop.first << ": \t" << *prop.second << std::endl;
+        // }
+        
+        // for (auto i = vout->begin(); i != vout->end(); ++i)
+        //     std::cout << *i << ' ';
+        
+        std::cout << "\nNumber of time points = " << vout->size() << std::endl;
+        data.close();
+    }catch (IncorrectChunk &exc){
+        std::cerr << "Exception caught " << exc.what() << "\n";
     }
-    
-    for (auto i = vout->begin(); i != vout->end(); ++i)
-        std::cout << *i << ' ';
-    
-    std::cout << "\nNumber of time points = " << vout->size() << std::endl;
-    data.close();
 }
